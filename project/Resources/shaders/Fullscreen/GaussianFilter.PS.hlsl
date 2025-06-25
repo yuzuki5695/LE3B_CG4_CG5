c759ed892deb5374ae531f61e07e7de08b2ff931
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-06-25 16:46:18 +0900

    CG5_00_05_更新中

---
+++ project/Resources/shaders/Fullscreen/GaussianFilter.PS.hlsl
@@ -0,0 +1,36 @@
+#include "Fullscreen.hlsli"
+static const float PI = 3.14159265358979323846f;
+
+Texture2D<float4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+struct PixeShaderOutput{
+    float4 color : SV_TARGET0;
+};
+
+float gauss(float x, float y, float sigma) {
+    float exponent = -(x * x + y * y) * rcp(2.0f * sigma * sigma);
+    float denominator = 2.0f * PI * sigma * sigma;
+    return exp(exponent) * rcp(denominator);
+};
+
+PixeShaderOutput main(VertexShaderOutput input){
+    PixeShaderOutput output;
+    // kenrnelを求める。weightは後で行う
+    float weight = 0.0f;
+    float kenrnel[3][3];
+    for (int32_t x = 0; x < 3; ++x) {
+        for (int32_t y = 0; y < 3; ++y) {
+            kenrnel[x][y] = gauss(kenrnel[x][y].x, kenrnel[x][y].y, 2.0f);
+            weight += kenrnel[x][y]; 
+        }
+    } 
+    // 求めたkenrnelを使い、BoxFilterと同じく畳み込みを行う。kKernel3x3を定数にしていたところがkenrnel3xに変わるだけである
+   
+    
+    
+    // 畳み込み後の値を正規化する。本来gauss関数は全体を合計すると (積分) 1になるように設計されている。しかし、無限の範囲は足せないので、
+    // kenrnel値の合計であるweightは1に満たない。なので、合計が1になるように逆数を掛けて全体を底上げして調整する
+    output.color.rgb *= rcp(weight);
+    return output;
+}
