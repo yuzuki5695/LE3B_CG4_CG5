f50dcd3b025333409746a119c2d44ea59c28b495
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-06-25 15:55:58 +0900

    CG5_00_02_更新中

--- project/Resources/shaders/Fullscreen.PS.hlsl
+++ project/Resources/shaders/Fullscreen.PS.hlsl
@@ -1,17 +1,18 @@
 #include "Fullscreen.hlsli"
 
-Texture2D<float32_t4> gTexture : register(t0);
+Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
 
 struct PixeShaderOutput{
-    float32_t4 color : SV_TARGET0;
+    float4 color : SV_TARGET0;
 }; 
 
 PixeShaderOutput main(VertexShaderOutput input){
     PixeShaderOutput output;
     output.color = gTexture.Sample(gSampler,input.texcoord);
-
+    // Grayscale(グレースケール化)
+    float value = dot(output.color.rgb, float3(0.2125f, 0.7154f, 0.0721f));
+    output.color.rgb = float3(value, value, value);
     return output;
-
 }
